655166598c7c283e07d8d3f984e959a813092853
Emmanuel Heffes <ema.heff@gmail.com>
2022-11-29 23:30:45 +0200

    adding file reader and question buielder

--- Maman13/Q1/src/Q2/Question.java
+++ Maman13/Q1/src/Q2/Question.java
@@ -1,14 +1,21 @@
 package Q2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Question {
 	private String q; 
-	private String correctAns; 
-	private ShuffleIterator<String> answersIterator;
+	private String correctAns;
+	private List<String> answers;
 	
-	public Question(String q, String correctAns, ShuffleIterator<String> answersIterator) {
+	public Question(String q, String correctAns, String ans2, String ans3, String ans4) {
 		this.q = q;
 		this.correctAns = correctAns;
-		this.answersIterator = answersIterator;
+		this.answers = new ArrayList<String>();
+		this.answers.add(correctAns);
+		this.answers.add(ans2);
+		this.answers.add(ans3);
+		this.answers.add(ans4);
 	}
 	
 	public String getQ() {
@@ -19,7 +26,7 @@ public class Question {
 		return correctAns;
 	}
 	
-	public ShuffleIterator<String> getAnswersIterator() {
-		return answersIterator;
+	public List<String> getAnswersIterator() {	
+		return answers;
 	}
 }
